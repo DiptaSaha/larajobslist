a32a5d0f3ec805df7e63b87fa65f179bc9b7f01c
Dipta saha <dipta196@gmail.com>
2023-03-22 20:32:44 +0600

    extra modify

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -3,20 +3,11 @@
 @include('partials._search')
 
 
-<a href="/" class="inline-block text-black ml-4 mb-4"
-                ><i class="fa-solid fa-arrow-left"></i> Back
-            </a>
+<a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back</a>
             <div class="mx-4">
               <x-card>
-                    <div
-                        class="flex flex-col items-center justify-center text-center"
-                    >
-                        <img
-                            class="w-48 mr-6 mb-6"
-                            src="{{$listing->logo?asset('storage/'.$listing->logo): asset('images/no-image.png')}}"
-                            alt=""
-                        />
-
+                    <div class="flex flex-col items-center justify-center text-center">
+                        <img class="w-48 mr-6 mb-6" src="{{$listing->logo?asset('storage/'.$listing->logo): asset('images/no-image.png')}}" alt=""/>
                         <h3 class="text-2xl mb-2">{{$listing->title}}</h3>
                         <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
                         <x-listing-tags :tagsCsv="$listing->tags"/>
@@ -30,17 +21,9 @@
                             <div class="text-lg space-y-6">
                                 <p>
                                    {{$listing->description}}
-                                </p>
-                               
-
-                                <a
-                                    href="mailto:.' {{$listing->email}}'"
-                                    class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"
-                                    ><i class="fa-solid fa-envelope"></i>
-                                    Contact Employer</a
-                                >
-
-                                <a href="{{$listing->website}}"                              target="_blank" class="block bg-black text-white py-2 rounded-xl hover:opacity-80"><i class="fa-solid fa-globe"></i> Visit Website
+                                </p>                              
+                                <a href="mailto:.' {{$listing->email}}'" class="block bg-laravel text-white mt-6 py-2 rounded-xl hover:opacity-80"><i class="fa-solid fa-envelope"></i>Contact Employer </a>
+                                <a href="{{$listing->website}}" target="_blank" class="block bg-black text-white py-2 rounded-xl hover:opacity-80"><i class="fa-solid fa-globe"></i> Visit Website
                                 </a>
                             </div>
                         </div>
